be52f8a903e92d6a5c6b673ee6c8a12dd41924c8
etamarrayner <etamarrayner@campus.technion.ac.il>
2026-05-27 00:01:03 +0300

    finish sysinfo, hard coded x86_64

--- Commands.cpp
+++ Commands.cpp
@@ -7,6 +7,7 @@
 #include <iomanip>
 #include "Commands.h"
 #include "sys/syscall.h"
+#include <fcntl.h>
 
 using namespace std;
 
@@ -120,6 +121,9 @@ Command *SmallShell::CreateCommand(const char *cmd_line) {
     else if (firstWord.compare("pwd") == 0) {
       return new GetCurrDirCommand(cmd_line);
     }
+    else if (firstWord.compare("sysinfo") == 0) {
+        return new SysInfoCommand(cmd_line);
+    }
     /*
     else if (firstWord.compare("showpid") == 0) {
       return new ShowPidCommand(cmd_line);
@@ -171,4 +175,55 @@ void GetCurrDirCommand::execute(){
     char* path = getcwd(NULL, 0);
     std::cout << path << std::endl;
     free(path);
+}
+
+void SysInfoCommand::execute() {
+    std::string keys[5] = {"System", "Hostname", "Kernel", "Architecture", "Boot Time"};
+    std::string values[5];
+    values[3] = "x86_64\n";
+    std::string suffixes[3] = {"ostype","hostname","osrelease"};
+    std::string kernel_path = "/proc/sys/kernel/";
+    std::string paths[4];
+    for(int i = 0; i < 3; i++) {
+        paths[i] = kernel_path + suffixes[i];
+    }
+    paths[3] = "/proc/stat";
+    for(int i = 0; i < 4; i++) {
+        char buffer[4096];
+        int fd = open((paths[i]).c_str(), O_RDONLY);
+        if(fd == -1) {
+            perror("smash error: open failed");
+            return;
+        }
+        int bytes_read = read(fd, buffer, sizeof(buffer)-1);
+        if(bytes_read == -1) {
+            perror("smash error: read failed");
+            close(fd);
+            return;
+        }
+        buffer[bytes_read] = '\0';
+        if(i != 3)
+            values[i] = buffer;
+        else {
+            time_t boot_time = 0;
+            char *btime_ptr = strstr(buffer, "btime");
+            if (btime_ptr != nullptr) {
+                sscanf(btime_ptr, "btime %ld", &boot_time);
+            }
+            if (boot_time > 0) {
+                struct tm *time_info = localtime(&boot_time);
+                char time_string[64];
+                strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", time_info);
+                values[4] = std::string(time_string) + '\n';
+            }
+        }
+        close(fd);
+    }
+    std::string ret = "";
+    for(int i = 0;i < 5;i++) {
+        ret += (keys[i] + ": " + values[i]);
+    }
+    write(1,ret.c_str(), ret.length());
+
+
 }
