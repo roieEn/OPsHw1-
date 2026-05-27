1e7b1536d807fb0b481807f79d73301b6b5b0e40
Roie Engelstein <roie4408@gmail.com>
2026-05-27 23:29:24 +0300

    change writing method in main from cout to write()

--- smash.cpp
+++ smash.cpp
@@ -13,7 +13,8 @@ int main(int argc, char *argv[]) {
 
     SmallShell &smash = SmallShell::getInstance();
     while (true) {
-        std::cout << smash.get_prompt() << "> ";
+        write(1, smash.get_prompt(), strlen(smash.get_prompt()));
+        write(1, "> ", 2);
         std::string cmd_line;
         std::getline(std::cin, cmd_line);
         smash.executeCommand(cmd_line.c_str());
